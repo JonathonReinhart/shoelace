7c287d1cbe321883456ceea63e87637880c7d181
Jonathon Reinhart <jrreinhart@google.com>
2023-04-14 19:49:10 +0000

    Install busybox into initrd

--- shoelace/cli.py
+++ shoelace/cli.py
@@ -1,10 +1,13 @@
 import argparse
+import logging
+import os
 from pathlib import Path
 import platform
+import shutil
 import tempfile
 from typing import IO
 
-from .initrd import InitRD
+from .initrd import InitRD, install_busybox
 from .qemu import run_qemu
 
 def _build_initrd(
@@ -12,19 +15,53 @@ def _build_initrd(
     file: IO[bytes],
 ) -> None:
     with InitRD(file) as initrd:  # flushed and closed on exit
-        initrd.add_file("/init", args.initprog)  # TODO: mode?
+        install_busybox(initrd, args.busybox)
+
+        if args.init:
+            initrd.add_file("/init", args.init)
+        else:
+            initrd.add_symlink(path="/init", target="/bin/busybox")
+
+
+def _readable_file_path(string: str) -> Path:
+    p = Path(string)
+    if not p.exists():
+        raise argparse.ArgumentTypeError(f"File not found: {p}")
+    if not p.is_file():
+        raise argparse.ArgumentTypeError(f"Not a file: {p}")
+    if not os.access(p, os.R_OK):
+        raise argparse.ArgumentTypeError(f"File not readable: {p}")
+    return p
+
+
+def setup_logging(args: argparse.Namespace) -> None:
+    # TODO
+    logging.basicConfig(level=logging.INFO)
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
-    # XXX temp
-    parser.add_argument('initprog', type=Path)
+    parser.add_argument("-i", "--init", type=_readable_file_path,
+                        help="Program to run as init; if not given, busybox is used")
+    parser.add_argument("--busybox", type=_readable_file_path,
+                        help="Path to statically-linked busybox binary")
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if args.busybox is None:
+        busybox = shutil.which("busybox")
+        if busybox:
+            args.busybox = _readable_file_path(busybox)
+        else:
+            args.error("busybox not provided or found")
+
+    return args
+
 
 def main() -> None:
     args = parse_args()
+    setup_logging(args)
 
     # Boot the host kernel
     kernel_bzimage = Path("/boot/vmlinuz-" + platform.release())
@@ -32,7 +69,7 @@ def main() -> None:
     kernel_args = [
         'quiet',
         'console=ttyS0',
-        #'root=/dev/ram0', #'rw',
+		'rdinit=/init',
     ]
 
     VM_CID = 7
--- shoelace/initrd.py
+++ shoelace/initrd.py
@@ -1,19 +1,51 @@
 from __future__ import annotations
 import contextlib
+from enum import IntEnum
 import logging
 from pathlib import Path
+import subprocess
 from typing import Any, IO
 
 # https://github.com/Changaco/python-libarchive-c
 # Debian: `apt install python3-libarchive-c`
 import libarchive  # type: ignore[import]
+import libarchive.entry  # type: ignore[import]
+import libarchive.ffi as _laffi  # type: ignore[import]
 
 _log = logging.getLogger(__name__)
+
+
+# TODO:
+# python-libarchive-c added a linkname setter and entry.FileType enum in
+# version 4.0, but Debian 11 ships version 2.9, so we hack this in ourselves.
+
+# These add methods to libarchive.ffi namespace
+
+# voidarchive_entry_set_symlink_type(struct archive_entry *entry, int type);
+_laffi.ffi('entry_set_symlink_type', [_laffi.c_archive_entry_p, _laffi.c_int], None)
+
+# void archive_entry_set_symlink_utf8(struct archive_entry *entry, const char *linkname);
+_laffi.ffi('entry_set_symlink_utf8', [_laffi.c_archive_entry_p, _laffi.c_char_p], None)
+
+class SymlinkType(IntEnum):
+	FILE = 1
+	DIRECTORY = 2
+
+class FileType(IntEnum):
+    NAMED_PIPE     = AE_IFIFO  = 0o010000  # noqa: E221
+    CHAR_DEVICE    = AE_IFCHR  = 0o020000  # noqa: E221
+    DIRECTORY      = AE_IFDIR  = 0o040000  # noqa: E221
+    BLOCK_DEVICE   = AE_IFBLK  = 0o060000  # noqa: E221
+    REGULAR_FILE   = AE_IFREG  = 0o100000  # noqa: E221
+    SYMBOLIC_LINK  = AE_IFLNK  = 0o120000  # noqa: E221
+    SOCKET         = AE_IFSOCK = 0o140000  # noqa: E221
 
 
 class InitRD:
     def __init__(self, file: IO[bytes]):
         self._exit_stack = contextlib.ExitStack()
+
+        self._dirs_added: set[str] = set()
 
         MODE = "wb"
         if file.mode != MODE:
@@ -52,7 +84,7 @@ class InitRD:
         if not isinstance(path, Path):
             path = Path(path)
         if not path.is_absolute():
-            raise ValueError("path must be absolute")
+            raise ValueError(f"path must be absolute: {path!r}")
         name = str(path)[1:]
 
         if isinstance(contents, Path):
@@ -70,9 +102,105 @@ class InitRD:
 
         _log.debug("Adding file to initrd: name=%s size=%d permission=%s", name, len(data), permission)
 
+        self._create_directories_for(path)
         self._archive.add_file_from_memory(
             entry_path=name,
             entry_size=len(data),
             entry_data=data,
             permission=permission,
         )
+
+
+    def _create_directories_for(self, path: Path):
+        assert path.is_absolute()
+        for dirp in reversed(path.parents):
+            if dirp == Path("/"):
+                continue
+            self._create_directory(dirp)
+
+    def _create_directory(self, path: Path, perm: int = 0o755):
+        assert path.is_absolute()
+        name = str(path)[1:]
+        if name in self._dirs_added:
+            return
+        self._dirs_added.add(name)
+
+        _log.debug("Creating directory in initrd: %s", name)
+
+        with libarchive.entry.new_archive_entry() as entry_p:
+            entry = libarchive.entry.ArchiveEntry(None, entry_p)
+            entry.pathname = name
+            _laffi.entry_set_filetype(entry_p, FileType.DIRECTORY)
+            _laffi.entry_set_perm(entry_p, perm)
+            _laffi.entry_set_size(entry_p, 0)
+
+            _laffi.write_header(self._archive._pointer, entry_p)
+            _laffi.write_finish_entry(self._archive._pointer)
+
+
+    def add_symlink(
+        self,
+        path: str | Path,
+        target: str | Path,
+    ) -> None:
+        """Adds a symlink at path, pointing to target."""
+        # TODO: DRY with add_file
+        if not isinstance(path, Path):
+            path = Path(path)
+        if not path.is_absolute():
+            raise ValueError(f"path must be absolute: {path!r}")
+        name = str(path)[1:]
+
+        self._create_directories_for(path)
+
+        _log.debug("Adding symlink to initrd: %s -> %s", name, target)
+
+        # TODO: linkname setter and libarchive.entry.FileType enum were added
+        # in 4.0 but Debian11 ships 2.9, so we have to hack it in ourselves
+        #self._archive.add_file_from_memory(
+        #    entry_path=name,
+        #    entry_size=0,
+        #    entry_data=b"",
+        #    filetype=libarchive.entry.FileType.SYMBOLIC_LINK,
+        #    linkpath=str(target),
+        #)
+
+        with libarchive.entry.new_archive_entry() as entry_p:
+            entry = libarchive.entry.ArchiveEntry(None, entry_p)
+            entry.pathname = name
+
+            # https://github.com/libarchive/libarchive/blob/0fd2ed25d7/libarchive/test/test_write_disk_symlink.c#L223
+            _laffi.entry_set_filetype(entry_p, FileType.SYMBOLIC_LINK)
+            _laffi.entry_set_perm(entry_p, 0o777)
+            _laffi.entry_set_symlink_type(entry_p, SymlinkType.FILE)
+            _laffi.entry_set_size(entry_p, 0)
+
+            target_path_utf8 = str(target).encode('utf-8')
+            _laffi.entry_set_symlink_utf8(entry_p, _laffi.c_char_p(target_path_utf8))
+
+            _laffi.write_header(self._archive._pointer, entry_p)
+            _laffi.write_finish_entry(self._archive._pointer)
+
+
+
+def install_busybox(initrd: InitRD, busybox: Path) -> None:
+    # Ensure Busybox is statically-linked
+    # TODO: Another way to do this without shelling out to file?
+    output = subprocess.check_output(["file", str(busybox)], text=True)
+    if not "statically linked" in output:
+        raise Exception(f"Not statically-linked: {busybox}")
+
+    installed_path = Path("/bin/busybox")
+
+    initrd.add_file(installed_path, busybox)
+
+    output = subprocess.check_output([str(busybox), "--list-full"], text=True)
+    for app_path in output.splitlines():
+        app_path = "/" + app_path
+        _log.debug("Busybox app: %s", app_path)
+
+        # XXX TODO: check for existing entries in archive?
+        if "busybox" in app_path:
+            continue
+
+        initrd.add_symlink(path=app_path, target=installed_path)
